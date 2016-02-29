62d14cb37a0da1fbfbb600beba5d08f0169287ce
Zhmayev Yaroslav <salaros@yandex.ru>
2016-02-29 14:08:06 +0300

    adding Advanced Custom Fields plugin + its initialization via WP CLI

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Re-create the DB using stored config
 vendor/bin/wp db drop --yes
 vendor/bin/wp db create
+
+# Create Wordpress tables
 vendor/bin/wp core install --title=Example --admin_user=webmaster --admin_password=webmaster --admin_email=webmaster@example.org --skip-email
+
+# Activate required plugins
 vendor/bin/wp plugin activate timber-library
+vendor/bin/wp plugin activate advanced-custom-fields
