530a6f44f5a4e4e8eacb5cc9f3e71013a8917e99
Zhmayev Yaroslav <salaros@yandex.ru>
2016-03-04 02:21:22 +0300

    adding cron job template, later will be used to run WP Cron tasks via crontab

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -5,8 +5,12 @@ vendor/bin/wp db drop --yes
 vendor/bin/wp db create
 
 # Create Wordpress tables
-vendor/bin/wp core install --title=Example --admin_user=webmaster --admin_password=webmaster --admin_email=webmaster@example.org --skip-email
+vendor/bin/wp core install --title=Example --admin_user=webmaster --admin_password=webmaster --admin_email=webmaster@example.org --url=example.org --skip-email
 
 # Activate required plugins
 vendor/bin/wp plugin activate timber-library
 vendor/bin/wp plugin activate advanced-custom-fields
+
+# Process the cron job template
+wpRoot=$(pwd)
+sed "s|WP_ROOT|${wpRoot}/public|g" conf/cron.d/wptpl-cron-job.example > conf/cron.d/wptpl-cron-job
