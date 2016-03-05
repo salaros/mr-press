e8481d2b7adae9d1973995f92581c7ba097c97cf
Zhmayev Yaroslav <salaros@yandex.ru>
2016-03-05 22:59:35 +0300

    avoid using sudo command if running create-cron-jon script as root

--- scripts/create-cron-job.sh
+++ scripts/create-cron-job.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Process the cron job template
 wpRoot=${PWD}
+jobName=${PWD##*/}
+if [[ $(id -u) -ne 0 ]] ; then sudoPrefix=sudo ; fi
+
+# Process the cron job template
 sed "s|WP_ROOT|${wpRoot}/public|g" conf/cron.d/wptpl-cron-job.example > conf/cron.d/wptpl-cron-job
 
-jobName=${PWD##*/}
-sudo cp -nv conf/cron.d/wptpl-cron-job /etc/cron.d/$jobName
+# Enable the newly created cron job
+$sudoPrefix cp -nv conf/cron.d/wptpl-cron-job /etc/cron.d/$jobName
