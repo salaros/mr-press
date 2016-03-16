51fd689919dbe0bcc33fdb1387344098d56d7003
Zhmayev Yaroslav <salaros@yandex.ru>
2016-03-16 22:17:03 +0300

    adding some basic development and production environment settings

---
+++ conf/wordpress/environments/development.php
@@ -0,0 +1,10 @@
+<?php
+
+// PHP error settings
+error_reporting( E_ERROR | E_WARNING | E_PARSE );
+ini_set( 'display_errors',       1 );
+
+// Wordpress debug settings
+define( 'WP_DEBUG',              true );
+define( 'SAVEQUERIES',           true );
+define( 'SCRIPT_DEBUG',          true );
---
+++ conf/wordpress/environments/production.php
@@ -0,0 +1,10 @@
+<?php
+
+// PHP error settings
+error_reporting( E_ERROR );
+ini_set( 'display_errors',       0 );
+
+// Wordpress debug settings
+define( 'WP_DEBUG',              false );
+define( 'SAVEQUERIES',           false );
+define( 'SCRIPT_DEBUG',          false );
