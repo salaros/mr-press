10304eba7d8df89a58b68174141b54f7aae52745
salaros <salaros@yandex.ru>
2016-09-03 15:44:50 +0300

    fixed required statements in wp-config.php file

--- wp-config.php
+++ wp-config.php
@@ -1,10 +1,10 @@
 <?php
 
 /** Composer autoload script */
-require_once( 'vendor/autoload.php' );
+require_once 'vendor/autoload.php';
 
 /** Load the settings */
-require_once( 'conf/wordpress/bootstrap.php' );
+require_once 'conf/wordpress/bootstrap.php';
 
 /** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+require_once sprintf('%swp-settings.php', ABSPATH);
