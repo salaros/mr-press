6aa0f107dbac2f8db8e5d99b93c40fd8043adbf4
salaros <salaros@yandex.ru>
2017-06-03 16:24:18 +0300

    improved environment-specific settings
    by enabling debug.log, conditionally enabling script debug and save queries functionality

--- conf/wordpress/environments/development.php
+++ conf/wordpress/environments/development.php
@@ -1,10 +1,18 @@
 <?php
 
-// PHP error settings
-error_reporting( E_ERROR | E_WARNING | E_PARSE );
-ini_set( 'display_errors',       1 );
+// ** Allow switching on onscreen error reporting ** //
+// ** by using 'DEBUG' GET/POST variable ** //
+$debug_mode = isset( $_REQUEST['DEBUG'] )
+	? ( intval( $_REQUEST['DEBUG'] ) ?: 1 )
+	: 0;
 
-// Wordpress debug settings
-define( 'WP_DEBUG',              true );
-define( 'SAVEQUERIES',           true );
-define( 'SCRIPT_DEBUG',          true );
+// ** PHP error settings ** //
+error_reporting( E_ERROR | E_WARNING | E_PARSE | E_NOTICE );
+ini_set( 'display_errors',          $debug_mode > 0 );
+
+// ** Wordpress debug settings ** //
+define( 'WP_DEBUG',                 getenv( 'WP_DEBUG_DISPLAY' ) ?: true );
+define( 'WP_DEBUG_DISPLAY',         $debug_mode > 0 || getenv( 'WP_DEBUG_DISPLAY' ) );
+define( 'WP_DEBUG_LOG',             $debug_mode < 1 || getenv( 'WP_DEBUG_LOG' ) );
+define( 'SAVEQUERIES',              3 === $debug_mode || getenv( 'SAVEQUERIES' ) );
+define( 'SCRIPT_DEBUG',             2 === $debug_mode || getenv( 'SCRIPT_DEBUG' ) );
--- conf/wordpress/environments/production.php
+++ conf/wordpress/environments/production.php
@@ -1,10 +1,12 @@
 <?php
 
-// PHP error settings
-error_reporting( E_ERROR );
+// ** PHP error settings ** //
+error_reporting( E_ERROR | E_PARSE );
 ini_set( 'display_errors',       0 );
 
-// Wordpress debug settings
-define( 'WP_DEBUG',              false );
-define( 'SAVEQUERIES',           false );
-define( 'SCRIPT_DEBUG',          false );
+// ** Wordpress debug settings ** //
+define( 'WP_DEBUG',              getenv( 'WP_DEBUG' ) ?: true );
+define( 'WP_DEBUG_DISPLAY',      getenv( 'WP_DEBUG_DISPLAY' ) ?: false );
+define( 'WP_DEBUG_LOG',          getenv( 'WP_DEBUG_LOG' ) ?: true );
+define( 'SAVEQUERIES',           getenv( 'SAVEQUERIES' ) ?: false );
+define( 'SCRIPT_DEBUG',          getenv( 'SCRIPT_DEBUG' ) ?: false );
